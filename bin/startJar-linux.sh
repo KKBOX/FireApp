0613aff8a66fd82233e311c11ac026113239ce80
tka.lu <tkatka@gmail.com>
2012-06-20 01:41:04 +0800

    in linux dev. script remove jruby.reify.classes=true

--- bin/startJar-linux.sh
+++ bin/startJar-linux.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-if [ "$1" = "full" ]; then
-  rake rawr:clean
-fi 
+rake rawr:clean
 rake rawr:jar
 
 #"-Djruby.compat.version=1.9" \
-java -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Djruby.reify.classes=true  \
--Xms512m -Xmn256m \
+#java -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Djruby.reify.classes=true  \
+java -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails  \
 -client -Xverify:none  -Xbootclasspath/a:lib/java/jruby-complete.jar -jar package/jar/fire-app.jar $1
