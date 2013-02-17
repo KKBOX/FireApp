611f77b12af461106b593443424d946bf96b39e0
tka.lu <tkatka@gmail.com>
2013-02-18 01:41:10 +0800

    watch coffeescript

--- src/app_watcher.rb
+++ src/app_watcher.rb
@@ -3,13 +3,29 @@ module Compass
     class  AppWatcher < ProjectWatcher
       def initialize(project_path, watches=[], options={}, poll=false)
         super
-        @sass_watchers +=[]
+        @sass_watchers += coffeescript_watchers
         setup_listener
       end
 
       def watch!
         compile
         super
+      end
+
+      def coffeescript_watchers
+        coffee_filter = File.join(Compass.configuration.fireapp_coffeescripts_dir,  "*.coffee")
+        child_coffee_filter = File.join(Compass.configuration.fireapp_coffeescripts_dir, "**", "*.coffee")
+
+        [ Watcher::Watch.new(child_coffee_filter, &method(:coffee_callback) ),
+          Watcher::Watch.new(coffee_filter, &method(:coffee_callback) ) ]
+      end
+
+      def coffee_callback(base, file, action)
+        log_action(:info, "#{file} was #{action}", options)
+        puts( "#{file} was #{action}", options)
+        CoffeeCompiler.compile_folder( Compass.configuration.fireapp_coffeescripts_dir,
+                                       Compass.configuration.javascripts_dir, 
+                                       Compass.configuration.fireapp_coffeescript_options );
       end
     end
   end
