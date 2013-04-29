ad06c98ed837e899840e6239ce2686122e69cbf0
tka.lu <tkatka@gmail.com>
2013-04-30 00:11:22 +0800

    update rawr

--- src/compile_version.rb
+++ src/compile_version.rb
@@ -1,5 +1,10 @@
 	module CompileVersion
+<<<<<<< HEAD
 	REVISION = 'e6d27b9d0c'
 	COMPILE_TIME = '201304291426'
+=======
+	REVISION = '95af139f91'
+	COMPILE_TIME = '201304291610'
+>>>>>>> cd57afa... update rawr
   UPDATE_URL = ''
 	end
