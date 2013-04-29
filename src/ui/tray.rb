2f445fa5f675f944760364119e421c0ff1505c37
tka.lu <tkatka@gmail.com>
2013-04-29 10:17:34 +0800

    Revert "remove useless code"
    This reverts commit e994bc8571c0940de1b12902f7a38c29f4fb7443.

--- src/ui/tray.rb
+++ src/ui/tray.rb
@@ -599,6 +599,7 @@ class Tray
       logger = Compass::Logger.new({ :display => App.display, :log_dir => dir})
       Compass.reset_configuration!
       Dir.chdir(dir)
+      x = Compass::Commands::UpdateProject.new( dir, {:logger => logger})
 
       Thread.abort_on_exception = true
       @compass_thread = Thread.new do
