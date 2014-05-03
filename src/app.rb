2b7821bc957e461b71abd5e0834acc84b40ba6c6
HondaDai <hondadai.tw@gmail.com>
2014-05-03 17:13:09 +0800

    update default use_version = 1.0

--- src/app.rb
+++ src/app.rb
@@ -69,7 +69,7 @@ module App
                                 
     config={
       "show_welcome" => true,
-      "use_version" => 0.12,
+      "use_version" => 1.0,
       "use_specify_gem_path" => false,
       "notifications" => [ :error, :warning ],
       "save_notification_to_file" => true,
