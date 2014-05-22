c39ad92eca9ee03899b9ac4e7738d50d7209a88e
HondaDai <hondadai.tw@gmail.com>
2014-05-22 23:30:40 +0800

    compilation cache get/update

--- src/compiler/compilation_cache.rb
+++ src/compiler/compilation_cache.rb
@@ -1,17 +1,38 @@
 
+require 'time'
 require 'fileutils'
 
 class CompilationCache
 
-  def initialize
+  def initialize(cache_folder_name)
     if not defined?(App)
       raise "You shoulod have App"
     end
 
-    @cache_dir = File.expand_path( File.join(Compass.configuration.project_path, self.cache_folder_name))
+    @cache_dir = File.expand_path( File.join(Compass.configuration.project_path, cache_folder_name))
     FileUtils.mkdir_p(@cache_dir) unless  File.exists?(@cache_dir)
   end
 
-  
+  def get(file_name)
+    cache_file = get_cache_file(file_name)
+    cache_object = JSON.load( cache_file.read ) if cache_file.file?
+
+    return cache_object["content"] if cache_object && cache_object["mtime"] == file_name.mtime.to_i
+    return nil
+  end
+
+  def update(file_name, content)
+    cache_file = get_cache_file(file_name)
+    cache_file.open('w') do|f|
+      f.write JSON.dump({"mtime" => cache_file.mtime.to_i, "content" => content})
+    end
+  end
+
+  private
+
+    def get_cache_file(file_name)
+      @cache_dir + file_name.to_s.gsub(/[^a-z0-9]/,"_")
+    end
+
 
 end
