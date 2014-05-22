6f32b8084a8a3cc47cc98c793c4f0819dbe6cd05
HondaDai <hondadai.tw@gmail.com>
2014-05-22 23:58:21 +0800

    coffee_compiler

---
+++ src/compiler/coffee_compiler.rb
@@ -0,0 +1,28 @@
+
+require 'base_compiler'
+require 'coffee_script'
+
+
+class CoffeeCompiler < BaseCompiler
+
+  def self.src_file_ext
+    "coffee"
+  end
+
+  def self.dst_file_ext
+    "js"
+  end
+
+  def self.cache_folder_name
+    ".coffeescript-cache"
+  end
+
+  def self.compile(src_file_path, dst_file_path, options = {})
+
+    self._compile(src_file_path, dst_file_path, options) do 
+      CoffeeScript.compile src_file_path.read, options
+    end
+
+  end
+
+end
