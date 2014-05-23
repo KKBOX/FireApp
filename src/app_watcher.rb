7b83494e12dbdb68f5237186f6a5fe7f60114637
HondaDai <hondadai.tw@gmail.com>
2014-05-23 23:30:08 +0800

    listen less compiler

--- src/app_watcher.rb
+++ src/app_watcher.rb
@@ -24,6 +24,7 @@ module Compass
         
         @watchers += custom_watcher(Compass.configuration.fireapp_coffeescripts_dir, "*.coffee", method(:coffee_callback))
         @watchers += custom_watcher(Compass.configuration.fireapp_livescripts_dir, "*.ls", method(:livescript_callback))
+        @watchers += custom_watcher(Compass.configuration.fireapp_less_dir, "*.less", method(:less_callback))
 
         setup_listener
       end
@@ -98,6 +99,16 @@ module Compass
                                       Compass.configuration.fireapp_livescript_options );
       end
 
+
+
+      def less_callback(base, file, action)
+        log_action(:info, "#{file} was #{action}", options)
+        puts( "#{file} was #{action}", options)
+        LessCompiler.compile_folder( Compass.configuration.fireapp_less_dir,
+                                      Compass.configuration.css_dir, 
+                                      Compass.configuration.fireapp_less_options );
+      end
+
       def livereload_watchers
        Watcher::LivereloadWatch.new(::App::CONFIG["services_livereload_extensions"], &method(:livereload_callback))
       end
@@ -113,22 +124,6 @@ module Compass
         tray = Tray.instance
         tray.shell.display.wake if tray.shell
       end 
-
-      def less_watchers
-        filter = File.join(Compass.configuration.fireapp_less_dir,  "*.less")
-        child_filter = File.join(Compass.configuration.fireapp_less_dir, "**", "*.less")
-
-        [ Watcher::Watch.new(child_filter, &method(:less_callback) ),
-          Watcher::Watch.new(filter, &method(:less_callback) ) ]
-      end
-
-      def less_callback(base, file, action)
-        log_action(:info, "#{file} was #{action}", options)
-        puts( "#{file} was #{action}", options)
-        LessCompiler.compile_folder( Compass.configuration.fireapp_less_dir,
-                                      Compass.configuration.css_dir, 
-                                      Compass.configuration.fireapp_less_options );
-      end
 
 
       def setup_listener
