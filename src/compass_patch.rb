a861a2dd80262cf043825fd81cb491c7e3d1fa9e
HondaDai <hondadai.tw@gmail.com>
2014-05-24 00:03:33 +0800

    rename method name

--- src/compass_patch.rb
+++ src/compass_patch.rb
@@ -104,7 +104,7 @@ module Compass
           LiveScriptCompiler.clean_folder(Compass.configuration.fireapp_livescripts_dir, Compass.configuration.javascripts_dir )
         end
         if File.exists?( Compass.configuration.fireapp_less_dir )
-          LessCompiler.clean_compile_folder(Compass.configuration.fireapp_less_dir, Compass.configuration.css_dir )
+          LessCompiler.clean_folder(Compass.configuration.fireapp_less_dir, Compass.configuration.css_dir )
         end
         compiler = new_compiler_instance
         compiler.clean!
