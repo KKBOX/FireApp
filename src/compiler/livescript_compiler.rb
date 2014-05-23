c55c46eb1e6670c70fc140fe77fdddae5b8989b4
HondaDai <hondadai.tw@gmail.com>
2014-05-23 12:07:02 +0800

    add livescript compiler

---
+++ src/compiler/livescript_compiler.rb
@@ -0,0 +1,27 @@
+
+require 'coffee_script'
+require 'pathname'
+
+class LiveScriptCompiler < BaseCompiler
+
+  def self.src_file_ext
+    "ls"
+  end
+
+  def self.dst_file_ext
+    "js"
+  end
+
+  def self.cache_folder_name
+    ".livescript-cache"
+  end
+
+  def self.compile(src_file_path, dst_file_path, options = {})
+
+    self._compile(src_file_path, dst_file_path, options) do 
+      LiveScript.compile Pathname.new(src_file_path).read, options
+    end
+
+  end
+
+end
