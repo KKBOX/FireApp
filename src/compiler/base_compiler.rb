653919d088943024857f1ef79f48e7fd85b2429e
HondaDai <hondadai.tw@gmail.com>
2014-05-23 09:52:56 +0800

    arrange

--- src/compiler/base_compiler.rb
+++ src/compiler/base_compiler.rb
@@ -2,58 +2,9 @@
 
 class BaseCompiler
 
-  def self.log(type, msg)
-    msg = msg.sub(File.expand_path(Compass.configuration.project_path), '')[1..-1] if defined?(Tray) 
-
-    if defined?(Tray) && Tray.instance.logger
-      Tray.instance.logger.record type, msg
-    else  
-      puts "   #{type} #{msg}"
-    end
-  end
-
-  def self._compile(src_file_path, dst_file_path, options)
-    src_file_path = Pathname.new(src_file_path)
-    dst_file_path = Pathname.new(dst_file_path)
-
-    cache = CompilationCache.new(cache_folder_name)
-
-    begin
-
-      content = cache.get(src_file_path)
-
-      if content.nil?
-        content = yield
-        cache.update(src_file_path, content)
-      end
-
-      write_dst_file(dst_file_path, content)
-      content
-
-    rescue Exception => e
-      error_msg = "#{src_file_path}: #{e.message}"
-      log(:error, error_msg)
-
-      "document.write("+ error_msg.to_json + ")"
-    end
-
-  end
-
-  def self.compile
+  def self.compile(src_file_path, dst_file_path, options = {})
+    # _compile(src_file_path, dst_file_path, options) do ... end
     raise "You should implement this method: #{__method__}"
-  end
-
-  def self.write_dst_file(dst_file_path, content)
-    dst_file_path.parent.mkdir unless dst_file_path.parent.exist?
-    if dst_file_path.exist?
-      log( :overwrite, dst_file_path.to_s)
-    else
-      log( :create, dst_file_path.to_s)
-    end
-
-    dst_file_path.open("w") do |f| 
-      f.write(content)
-    end
   end
 
   def self.src_file_ext
@@ -70,7 +21,6 @@ class BaseCompiler
     # "coffeescript-cache"
     raise "You should implement this method: #{__method__}"
   end
-
 
   def self.compile_folder(src_dir, dst_dir, options = {})
     src_dir = File.expand_path(src_dir)
@@ -91,30 +41,90 @@ class BaseCompiler
     end
   end
 
-  # def self.cache_dir
-  #   if not defined?(App)
-  #     raise "You shoulod have App"
-  #   end
-
-  #   dir = File.expand_path( File.join(Compass.configuration.project_path, self.cache_folder_name))
-  #   FileUtils.mkdir_p(dir) unless  File.exists?(dir)
-  #   return dir
-
-  # end
-
-  def self.get_dst_file_path(src_dir, src_file_path, dst_dir)
-    src_file_path = File.expand_path(src_file_path)
-
-    new_dir  = File.dirname(src_file_path.to_s.sub(src_dir, ""))
-    new_file = File.basename(src_file_path).gsub(/\.#{self.src_file_ext}/,".#{self.dst_file_ext}").gsub(/#{self.dst_file_ext}\.#{self.dst_file_ext}/,"#{self.dst_file_ext}")
-    
-    return  File.join(dst_dir, new_dir, new_file)
-  end
-
   def self.clean_folder(src_dir, dst_dir)
     src_dir = File.expand_path(src_dir)
     dst_dir = File.expand_path(dst_dir)
+
+    cache_dir = get_cache_dir(coffeescripts_dir)
+    FileUtils.rm_rf(cache_dir)
+    CoffeeCompiler.log( :remove, "#{cache_dir}/")
+
+    Dir.glob( File.join(coffeescripts_dir, "**", "*.coffee")) do |full_path|
+      new_js_path = get_new_js_path(coffeescripts_dir, full_path, javascripts_dir)
+      if File.exists?(new_js_path)
+        CoffeeCompiler.log( :remove, new_js_path)
+        FileUtils.rm_rf(new_js_path)
+      end 
+    end
   end
+
+
+  private
+
+
+    def self.log(type, msg)
+      msg = msg.sub(File.expand_path(Compass.configuration.project_path), '')[1..-1] if defined?(Tray) 
+
+      if defined?(Tray) && Tray.instance.logger
+        Tray.instance.logger.record type, msg
+      else  
+        puts "   #{type} #{msg}"
+      end
+    end
+
+    def self.get_dst_file_path(src_dir, src_file_path, dst_dir)
+      src_file_path = File.expand_path(src_file_path)
+
+      new_dir  = File.dirname(src_file_path.to_s.sub(src_dir, ""))
+      new_file = File.basename(src_file_path).gsub(/\.#{self.src_file_ext}/,".#{self.dst_file_ext}").gsub(/#{self.dst_file_ext}\.#{self.dst_file_ext}/,"#{self.dst_file_ext}")
+      
+      return  File.join(dst_dir, new_dir, new_file)
+    end
+
+    def self.write_dst_file(dst_file_path, content)
+      dst_file_path.parent.mkdir unless dst_file_path.parent.exist?
+      if dst_file_path.exist?
+        log( :overwrite, dst_file_path.to_s)
+      else
+        log( :create, dst_file_path.to_s)
+      end
+
+      dst_file_path.open("w") do |f| 
+        f.write(content)
+      end
+    end
+
+    
+
+    def self._compile(src_file_path, dst_file_path, options)
+      src_file_path = Pathname.new(src_file_path)
+      dst_file_path = Pathname.new(dst_file_path)
+
+      cache = CompilationCache.new(cache_folder_name)
+
+      begin
+
+        content = cache.get(src_file_path)
+
+        if content.nil?
+          content = yield
+          cache.update(src_file_path, content)
+        end
+
+        write_dst_file(dst_file_path, content)
+        content
+
+      rescue Exception => e
+        error_msg = "#{src_file_path}: #{e.message}"
+        log(:error, error_msg)
+
+        "document.write("+ error_msg.to_json + ")"
+      end
+
+    end
+
+
+
 
 
 end
