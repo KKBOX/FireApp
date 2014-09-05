2661a03b5dc3c809b9da963dc11b791a39878560
tka.lu <tkatka@gmail.com>
2014-09-05 13:34:52 +0800

    bump version 1.13

--- src/app.rb
+++ src/app.rb
@@ -11,7 +11,7 @@ module App
   extend self
   
   include CompileVersion
-  VERSION = "1.13.beta.1"
+  VERSION = "1.13"
   OS = org.jruby.platform.Platform::OS 
   OS_VERSION = java.lang.System.getProperty("os.version")
 
